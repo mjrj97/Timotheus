4c76077e3e144ebe4391454a58c8b6ba8db03d32
Jesper <Jesperroager@gmail.com>
2021-02-19 22:19:55 +0100

    Add pdfSharp

--- Forms/Manager.cs
+++ Forms/Manager.cs
@@ -8,6 +8,13 @@ using System.Diagnostics;
 using System.IO;
 using System.Windows.Forms;
 
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+
+
 namespace Timotheus
 {
     public partial class MainWindow : Form
@@ -182,7 +189,29 @@ namespace Timotheus
 
         public static void ExportPDF(string filePath, string title, List<Event> events)
         {
+            // Create a new PDF document
+            PdfDocument document = new PdfDocument();
+            document.Info.Title = "Created with PDFsharp";
 
+            // Create an empty page
+            PdfPage page = document.AddPage();
+
+            // Get an XGraphics object for drawing
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+
+            // Create a font
+            XFont font = new XFont("Verdana", 20, XFontStyle.BoldItalic);
+
+            // Draw the text
+            gfx.DrawString("Hello, World!", font, XBrushes.Black,
+              new XRect(0, 0, page.Width, page.Height),
+              XStringFormats.Center);
+
+            // Save the document...
+            const string filename = "HelloWorld.pdf";
+            document.Save(filename);
+            // ...and start a viewer.
+            Process.Start(filename);
         }
     }
 }
