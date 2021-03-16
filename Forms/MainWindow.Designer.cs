8193c4a3bd06579aedd5cc5433a1ecd09fd093ec
mjrj97 <martin.jensen.1997@hotmail.com>
2021-03-16 15:43:57 +0100

    Added icon to .exe and basic accounting functionality.

--- Forms/MainWindow.Designer.cs
+++ Forms/MainWindow.Designer.cs
@@ -22,7 +22,7 @@ namespace Timotheus.Forms
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
             this.Calendar_View = new System.Windows.Forms.DataGridView();
             this.Calendar_StartColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -65,6 +65,10 @@ namespace Timotheus.Forms
             this.SFTP_HostBox = new System.Windows.Forms.TextBox();
             this.Members_Page = new System.Windows.Forms.TabPage();
             this.Members_View = new System.Windows.Forms.DataGridView();
+            this.Members_SinceColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Members_NameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Members_BirthdayColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Members_AddressColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ConsentForms_Page = new System.Windows.Forms.TabPage();
             this.ConsentForms_RemoveButton = new System.Windows.Forms.Button();
             this.ConsentForms_AddButton = new System.Windows.Forms.Button();
@@ -73,6 +77,17 @@ namespace Timotheus.Forms
             this.ConsentForms_DateColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ConsentForms_VersionColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ConsentForms_CommentColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Accounting_Page = new System.Windows.Forms.TabPage();
+            this.Accounting_RemoveButton = new System.Windows.Forms.Button();
+            this.Accounting_AddButton = new System.Windows.Forms.Button();
+            this.Accounting_View = new System.Windows.Forms.DataGridView();
+            this.Accounting_DateColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Accounting_AppendixColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Accounting_DescriptionColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Accounting_AccountNumberColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Accounting_InColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Accounting_OutColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Accounting_BalanceColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Settings_Page = new System.Windows.Forms.TabPage();
             this.Settings_InfoBox = new System.Windows.Forms.GroupBox();
             this.Settings_LogoBox = new System.Windows.Forms.TextBox();
@@ -96,10 +111,6 @@ namespace Timotheus.Forms
             this.TrayContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.TrayOpen = new System.Windows.Forms.ToolStripMenuItem();
             this.TrayClose = new System.Windows.Forms.ToolStripMenuItem();
-            this.Members_SinceColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Members_NameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Members_BirthdayColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Members_AddressColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.Calendar_View)).BeginInit();
             this.tabControl.SuspendLayout();
             this.Calendar_Page.SuspendLayout();
@@ -109,6 +120,8 @@ namespace Timotheus.Forms
             ((System.ComponentModel.ISupportInitialize)(this.Members_View)).BeginInit();
             this.ConsentForms_Page.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ConsentForms_View)).BeginInit();
+            this.Accounting_Page.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Accounting_View)).BeginInit();
             this.Settings_Page.SuspendLayout();
             this.Settings_InfoBox.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Settings_PictureBox)).BeginInit();
@@ -129,14 +142,14 @@ namespace Timotheus.Forms
             this.Calendar_NameColumn,
             this.Calendar_DescriptionColumn,
             this.Calendar_LocationColumn});
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.Calendar_View.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.Calendar_View.DefaultCellStyle = dataGridViewCellStyle1;
             this.Calendar_View.Location = new System.Drawing.Point(10, 39);
             this.Calendar_View.Name = "Calendar_View";
             this.Calendar_View.RightToLeft = System.Windows.Forms.RightToLeft.No;
@@ -199,6 +212,7 @@ namespace Timotheus.Forms
             this.tabControl.Controls.Add(this.SFTP_Page);
             this.tabControl.Controls.Add(this.Members_Page);
             this.tabControl.Controls.Add(this.ConsentForms_Page);
+            this.tabControl.Controls.Add(this.Accounting_Page);
             this.tabControl.Controls.Add(this.Settings_Page);
             this.tabControl.Controls.Add(this.Help_Page);
             this.tabControl.Location = new System.Drawing.Point(2, 3);
@@ -587,6 +601,34 @@ namespace Timotheus.Forms
             this.Members_View.Size = new System.Drawing.Size(776, 346);
             this.Members_View.TabIndex = 0;
             // 
+            // Members_SinceColumn
+            // 
+            this.Members_SinceColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Members_SinceColumn.DataPropertyName = "memberSince";
+            this.Members_SinceColumn.HeaderText = "Member since";
+            this.Members_SinceColumn.Name = "Members_SinceColumn";
+            // 
+            // Members_NameColumn
+            // 
+            this.Members_NameColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Members_NameColumn.DataPropertyName = "name";
+            this.Members_NameColumn.HeaderText = "Name";
+            this.Members_NameColumn.Name = "Members_NameColumn";
+            // 
+            // Members_BirthdayColumn
+            // 
+            this.Members_BirthdayColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Members_BirthdayColumn.DataPropertyName = "birthday";
+            this.Members_BirthdayColumn.HeaderText = "Birthday";
+            this.Members_BirthdayColumn.Name = "Members_BirthdayColumn";
+            // 
+            // Members_AddressColumn
+            // 
+            this.Members_AddressColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Members_AddressColumn.DataPropertyName = "address";
+            this.Members_AddressColumn.HeaderText = "Address";
+            this.Members_AddressColumn.Name = "Members_AddressColumn";
+            // 
             // ConsentForms_Page
             // 
             this.ConsentForms_Page.Controls.Add(this.ConsentForms_RemoveButton);
@@ -672,6 +714,107 @@ namespace Timotheus.Forms
             this.ConsentForms_CommentColumn.HeaderText = "Comment";
             this.ConsentForms_CommentColumn.MinimumWidth = 6;
             this.ConsentForms_CommentColumn.Name = "ConsentForms_CommentColumn";
+            // 
+            // Accounting_Page
+            // 
+            this.Accounting_Page.Controls.Add(this.Accounting_RemoveButton);
+            this.Accounting_Page.Controls.Add(this.Accounting_AddButton);
+            this.Accounting_Page.Controls.Add(this.Accounting_View);
+            this.Accounting_Page.Location = new System.Drawing.Point(4, 24);
+            this.Accounting_Page.Name = "Accounting_Page";
+            this.Accounting_Page.Padding = new System.Windows.Forms.Padding(3);
+            this.Accounting_Page.Size = new System.Drawing.Size(794, 409);
+            this.Accounting_Page.TabIndex = 5;
+            this.Accounting_Page.Text = "Accounting";
+            this.Accounting_Page.UseVisualStyleBackColor = true;
+            // 
+            // Accounting_RemoveButton
+            // 
+            this.Accounting_RemoveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Accounting_RemoveButton.Location = new System.Drawing.Point(91, 379);
+            this.Accounting_RemoveButton.Name = "Accounting_RemoveButton";
+            this.Accounting_RemoveButton.Size = new System.Drawing.Size(75, 23);
+            this.Accounting_RemoveButton.TabIndex = 2;
+            this.Accounting_RemoveButton.Text = "Remove";
+            this.Accounting_RemoveButton.UseVisualStyleBackColor = true;
+            this.Accounting_RemoveButton.Click += new System.EventHandler(this.RemoveTransaction);
+            // 
+            // Accounting_AddButton
+            // 
+            this.Accounting_AddButton.Location = new System.Drawing.Point(10, 379);
+            this.Accounting_AddButton.Name = "Accounting_AddButton";
+            this.Accounting_AddButton.Size = new System.Drawing.Size(75, 23);
+            this.Accounting_AddButton.TabIndex = 1;
+            this.Accounting_AddButton.Text = "Add";
+            this.Accounting_AddButton.UseVisualStyleBackColor = true;
+            this.Accounting_AddButton.Click += new System.EventHandler(this.AddTransaction);
+            // 
+            // Accounting_View
+            // 
+            this.Accounting_View.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Accounting_View.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Accounting_View.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Accounting_DateColumn,
+            this.Accounting_AppendixColumn,
+            this.Accounting_DescriptionColumn,
+            this.Accounting_AccountNumberColumn,
+            this.Accounting_InColumn,
+            this.Accounting_OutColumn,
+            this.Accounting_BalanceColumn});
+            this.Accounting_View.Location = new System.Drawing.Point(10, 10);
+            this.Accounting_View.Name = "Accounting_View";
+            this.Accounting_View.RowHeadersVisible = false;
+            this.Accounting_View.RowTemplate.Height = 25;
+            this.Accounting_View.Size = new System.Drawing.Size(776, 363);
+            this.Accounting_View.TabIndex = 0;
+            // 
+            // Accounting_DateColumn
+            // 
+            this.Accounting_DateColumn.DataPropertyName = "Date";
+            this.Accounting_DateColumn.HeaderText = "Date";
+            this.Accounting_DateColumn.Name = "Accounting_DateColumn";
+            this.Accounting_DateColumn.Width = 90;
+            // 
+            // Accounting_AppendixColumn
+            // 
+            this.Accounting_AppendixColumn.DataPropertyName = "Appendix";
+            this.Accounting_AppendixColumn.HeaderText = "Appendix";
+            this.Accounting_AppendixColumn.Name = "Accounting_AppendixColumn";
+            this.Accounting_AppendixColumn.Width = 70;
+            // 
+            // Accounting_DescriptionColumn
+            // 
+            this.Accounting_DescriptionColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Accounting_DescriptionColumn.DataPropertyName = "Description";
+            this.Accounting_DescriptionColumn.HeaderText = "Description";
+            this.Accounting_DescriptionColumn.Name = "Accounting_DescriptionColumn";
+            // 
+            // Accounting_AccountNumberColumn
+            // 
+            this.Accounting_AccountNumberColumn.DataPropertyName = "AccountNumber";
+            this.Accounting_AccountNumberColumn.HeaderText = "Account number";
+            this.Accounting_AccountNumberColumn.Name = "Accounting_AccountNumberColumn";
+            this.Accounting_AccountNumberColumn.Width = 75;
+            // 
+            // Accounting_InColumn
+            // 
+            this.Accounting_InColumn.DataPropertyName = "InValue";
+            this.Accounting_InColumn.HeaderText = "In";
+            this.Accounting_InColumn.Name = "Accounting_InColumn";
+            this.Accounting_InColumn.Width = 75;
+            // 
+            // Accounting_OutColumn
+            // 
+            this.Accounting_OutColumn.DataPropertyName = "OutValue";
+            this.Accounting_OutColumn.HeaderText = "Out";
+            this.Accounting_OutColumn.Name = "Accounting_OutColumn";
+            this.Accounting_OutColumn.Width = 75;
+            // 
+            // Accounting_BalanceColumn
+            // 
+            this.Accounting_BalanceColumn.DataPropertyName = "Balance";
+            this.Accounting_BalanceColumn.HeaderText = "Balance";
+            this.Accounting_BalanceColumn.Name = "Accounting_BalanceColumn";
             // 
             // Settings_Page
             // 
@@ -895,34 +1038,6 @@ namespace Timotheus.Forms
             this.TrayClose.Text = "Close";
             this.TrayClose.Click += new System.EventHandler(this.Exit);
             // 
-            // Members_SinceColumn
-            // 
-            this.Members_SinceColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Members_SinceColumn.DataPropertyName = "memberSince";
-            this.Members_SinceColumn.HeaderText = "Member since";
-            this.Members_SinceColumn.Name = "Members_SinceColumn";
-            // 
-            // Members_NameColumn
-            // 
-            this.Members_NameColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Members_NameColumn.DataPropertyName = "name";
-            this.Members_NameColumn.HeaderText = "Name";
-            this.Members_NameColumn.Name = "Members_NameColumn";
-            // 
-            // Members_BirthdayColumn
-            // 
-            this.Members_BirthdayColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Members_BirthdayColumn.DataPropertyName = "birthday";
-            this.Members_BirthdayColumn.HeaderText = "Birthday";
-            this.Members_BirthdayColumn.Name = "Members_BirthdayColumn";
-            // 
-            // Members_AddressColumn
-            // 
-            this.Members_AddressColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Members_AddressColumn.DataPropertyName = "address";
-            this.Members_AddressColumn.HeaderText = "Address";
-            this.Members_AddressColumn.Name = "Members_AddressColumn";
-            // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -945,6 +1060,8 @@ namespace Timotheus.Forms
             ((System.ComponentModel.ISupportInitialize)(this.Members_View)).EndInit();
             this.ConsentForms_Page.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.ConsentForms_View)).EndInit();
+            this.Accounting_Page.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Accounting_View)).EndInit();
             this.Settings_Page.ResumeLayout(false);
             this.Settings_InfoBox.ResumeLayout(false);
             this.Settings_InfoBox.PerformLayout();
@@ -1035,5 +1152,16 @@ namespace Timotheus.Forms
         private System.Windows.Forms.Label Help_SourceLabel;
         private System.Windows.Forms.LinkLabel Help_SourceLink;
         private System.Windows.Forms.PictureBox Help_IconBox;
+        private System.Windows.Forms.TabPage Accounting_Page;
+        private System.Windows.Forms.DataGridView Accounting_View;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Accounting_DateColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Accounting_AppendixColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Accounting_DescriptionColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Accounting_AccountNumberColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Accounting_InColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Accounting_OutColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Accounting_BalanceColumn;
+        private System.Windows.Forms.Button Accounting_RemoveButton;
+        private System.Windows.Forms.Button Accounting_AddButton;
     }
 }
---
+++ Utility/Transaction.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using Timotheus.Forms;
+
+namespace Timotheus.Utility
+{
+    public class Transaction
+    {
+        private DateTime date;
+        private double inValue;
+        private double outValue;
+
+        public DateTime Date
+        {
+            get
+            {
+                return date;
+            }
+            set
+            {
+                date = value;
+                AddTransaction();
+            }
+        }
+        public int Appendix { get; set; }
+        public string Description { get; set; }
+        public int AccountNumber { get; set; }
+        public double InValue
+        {
+            get
+            {
+                return inValue;
+            }
+            set
+            {
+                inValue = value;
+                UpdateBalance();
+            }
+        }
+        public double OutValue
+        {
+            get
+            {
+                return outValue;
+            }
+            set
+            {
+                outValue = value;
+                UpdateBalance();
+            }
+        }
+        public double Balance { get; private set; }
+
+        public static List<Transaction> list = new List<Transaction>();
+
+        public Transaction(DateTime Date, int Appendix, string Description, int AccountNumber, double InValue, double OutValue)
+        {
+            this.Date = Date;
+            this.Appendix = Appendix;
+            this.Description = Description;
+            this.AccountNumber = AccountNumber;
+            this.InValue = InValue;
+            this.OutValue = OutValue;
+        }
+
+        private void UpdateBalance()
+        {
+            double previousBalance = 0.0;
+            int i = list.IndexOf(this);
+            if (i > 0)
+                previousBalance = list[i - 1].Balance;
+            Balance = InValue - OutValue + previousBalance;
+            if (i < list.Count - 1)
+                list[i + 1].UpdateBalance();
+        }
+
+        private void AddTransaction()
+        {
+            int insertIndex = -1;
+            list.Remove(this);
+            if (list.Count >= 1)
+            {
+                if (list[0].date > date)
+                    insertIndex = 0;
+                else
+                {
+                    int i = 1;
+                    while (i < list.Count && insertIndex == -1)
+                    {
+                        if (list[i - 1].date <= date && list[i].date > date)
+                            insertIndex = i;
+                        i++;
+                    }
+                }
+            }
+            if (insertIndex == -1)
+                list.Add(this);
+            else
+                list.Insert(insertIndex, this);
+            if (MainWindow.window != null)
+            {
+                MainWindow.window.UpdateAccountingTable();
+                list[0].UpdateBalance();
+            }
+        }
+    }
+}
