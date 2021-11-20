56cd09e095787b95baea6f40d185a3a62ef1c85f
mjrj97 <martin.jensen.1997@hotmail.com>
2021-11-20 14:35:33 +0100

    Added documentation to EventViewModel. Fixed bug in MainWindow.

--- Manager/ViewModels/EventViewModel.cs
+++ Manager/ViewModels/EventViewModel.cs
@@ -5,8 +5,14 @@ namespace Timotheus.ViewModels
 {
     public class EventViewModel
     {
+        /// <summary>
+        /// Event the View Model takes data from.
+        /// </summary>
         private readonly Event ev;
 
+        /// <summary>
+        /// The start date of the event formatted.
+        /// </summary>
         public string Start
         {
             get
@@ -19,6 +25,20 @@ namespace Timotheus.ViewModels
             }
         }
 
+        /// <summary>
+        /// The start date of the event for sorting.
+        /// </summary>
+        public DateTime StartSort
+        {
+            get
+            {
+                return ev.Start;
+            }
+        }
+
+        /// <summary>
+        /// The end date of the event formatted.
+        /// </summary>
         public string End
         {
             get
@@ -31,6 +51,20 @@ namespace Timotheus.ViewModels
             }
         }
 
+        /// <summary>
+        /// The end date of the event for sorting.
+        /// </summary>
+        public DateTime EndSort
+        {
+            get
+            {
+                return ev.End;
+            }
+        }
+
+        /// <summary>
+        /// Name of the event.
+        /// </summary>
         public string Name
         {
             get
@@ -43,6 +77,9 @@ namespace Timotheus.ViewModels
             }
         }
 
+        /// <summary>
+        /// The events description.
+        /// </summary>
         public string Description
         {
             get
@@ -55,6 +92,9 @@ namespace Timotheus.ViewModels
             }
         }
 
+        /// <summary>
+        /// The location of the event.
+        /// </summary>
         public string Location
         {
             get
@@ -67,6 +107,9 @@ namespace Timotheus.ViewModels
             }
         }
 
+        /// <summary>
+        /// Whether the event is marked for deletion or not.
+        /// </summary>
         public bool Deleted
         {
             get
@@ -79,6 +122,9 @@ namespace Timotheus.ViewModels
             }
         }
 
+        /// <summary>
+        /// Creates a view model of an event for usage in UI.
+        /// </summary>
         public EventViewModel(Event ev)
         {
             this.ev = ev;
