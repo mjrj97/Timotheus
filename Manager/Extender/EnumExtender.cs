56977053eea1446bc4c5ccbfa6e1361ffe4b7db6
timi992000 <77628993+timi992000@users.noreply.github.com>
2023-04-24 00:47:13 +0200

    Fix GetStringValue

--- Manager/Extender/EnumExtender.cs
+++ Manager/Extender/EnumExtender.cs
@@ -9,9 +9,10 @@ namespace Timotheus.Extender
 		public static string GetStringValue(this Enum Value)
 		{
 			var t = Value.GetType();
-			var type = t.GetCustomAttributes(typeof(DescriptionAttribute), false);
-			if (type != null && type.Any())
-				return type.GetValue(0).ToString();
+			var fi = t.GetField(Value.ToString());
+			DescriptionAttribute[] attrs = fi.GetCustomAttributes(typeof(DescriptionAttribute), false) as DescriptionAttribute[];
+			if (attrs != null && attrs.Length > 0)
+				return attrs[0].Description;
 			return "";
 		}
 	}
